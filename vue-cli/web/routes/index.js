e921e83ec2181cc7b9fd2bd6991eb61fc0a92279
ZhangBaoCai <talent518@yeah.net>
2019-11-14 11:22:56 +0800

    web的默认首页提示vue的构建命令。

--- vue-cli/web/routes/index.js
+++ vue-cli/web/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', vuedir: path.join(__dirname, '../../') });
 });
 
 router.use('/api', require('./api'));
